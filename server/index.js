37c3ae3882ec936b64878b01474a894559e2dbd3
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-01-16 13:14:58 +0530

    ver 1.0

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
